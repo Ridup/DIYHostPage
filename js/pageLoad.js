93f605b0a04506558c1064196d008ae661aeddf4
Rido <2294907171@qq.com>
2017-09-26 23:52:16 +0800

    2017年9月26日23:50:56
    骨架完成！

---
+++ js/pageLoad.js
@@ -0,0 +1,80 @@
+/*
+* @Author: Marte
+* @Date:   2017-09-23 10:41:40
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-09-26 19:01:12
+*/
+
+'use strict';
+jQuery(document).ready(function($) {
+    //加载背景图片
+    /*alert("beginning...");
+    var strUrl = "";
+    var NewXmlDoc = loadXML("../xml/config.xml");
+    var NewTmpTag = NewXmlDoc.getElementsByTagName("background-images");
+    var strUrl = NewTmpTag[0].getElementsByTagName("now-image")[0].firstChild.nodeValue;
+    alert(strUrl);*/
+    //元素hover()特效
+    $(".changeImg").hover(function(){
+    $(".changeImg span").css("color","#1ADACF");
+},function(){
+    $(".changeImg span").css("color","white");
+});
+    $(".helpTip").hover(function(){
+    $(".helpTip span").css("color","#1ADACF");
+},function(){
+    $(".helpTip span").css("color","white");
+});
+
+});
+
+//加载xml文件
+/*function loadXML(xmlFile) {
+        alert("beginning...1");
+
+            var xmlDoc, xmlhttp;
+            if (window.XMLHttpRequest) {//兼容 IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            }
+            else {//兼容 IE5、IE6
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.open("GET", xmlFile, false);  //使用HTTP GET初始化HTTP请求
+            xmlhttp.send(); //发送HTTP请求并获取HTTP响应
+                alert("beginning...2");
+
+            xmlDoc = xmlhttp.responseXML;  //获取XML文档
+            return xmlDoc;
+}*/
+
+//背景图片切换
+function changeBg(){
+    var randomNumber = parseInt(Math.random()*(4050-1+1) + 1);
+    var imgBgUrl =
+    "background-image: url('https://img.infinitynewtab.com/wallpaper/"+randomNumber+".jpg');"
+    $(".page").attr("style",imgBgUrl);
+
+    // //保存图片路径
+    // XmlDocument xmlDoc=new XmlDocument();
+    // xmlDoc.Load("config.xml");
+    // XmlNode xn=xmlDoc.SelectSingleNode("background-images");
+    // XmlNodeList xnl=xn.ChildNodes;
+    // alert("message");
+    // XmlNode imgUrl=xnl[0];
+    // alert(imgUrl);
+    // XmlElement xe=(XmlElement)imgUrl;
+    // xe.SetAttribute("now-image",imgBgUrl);
+    // xmlDoc.Save("config.xml");//保存。
+    //
+
+
+
+
+
+}
+
+
+
+window.onload = function() {
+  document.getElementById("_easyui_textbox_input1").focus();
+};
---
+++ js/search.js
@@ -0,0 +1,135 @@
+/*
+* @Author: Marte
+* @Date:   2017-09-25 18:53:18
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-09-26 20:48:55
+*/
+
+'use strict';
+function baiduSearch(value,name){
+    var url = "";
+    if(name=="net"){
+        if(value!=""){
+            url="https://www.baidu.com/s?wd="+value;
+        }else{
+            url="https://www.baidu.com/";
+        }
+    }
+    if(name=="picture"){
+        if(value!=""){
+            url="http://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word="+value;
+        }else{
+            url="https://image.baidu.com/";
+        }
+    }
+    window.open(url,"_blank");
+}
+
+function googleSearch(value,name){
+    var url = "";
+    if(name=="net"){
+        if(value!=""){
+            url="https://www.google.com.hk/search?safe=strict&source=hp&q="+value;
+        }else{
+            url="https://www.google.com.hk/";
+        }
+    }
+    if(name=="picture"){
+        if(value!=""){
+            url="https://www.google.com.hk/search?q="+value+"&safe=strict&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi92vHE98HWAhWMTrwKHXjSA1kQ_AUICygC";
+        }else{
+            url="https://www.google.com.hk/imghp?hl=zh-CN&tab=wi";
+        }
+    }
+    window.open(url,"_blank");
+}
+
+function bingSearch(value,name){
+    var url = "";
+    if(name=="net"){
+        if(value!=""){
+            url="http://cn.bing.com/search?q="+value;
+        }else{
+            url="http://cn.bing.com/";
+        }
+    }
+    if(name=="picture"){
+        if(value!=""){
+            url="http://cn.bing.com/images/search?q="+value;
+        }else{
+            url="http://cn.bing.com/images/trending?form=HDRSC2";
+        }
+    }
+    window.open(url,"_blank");
+}
+
+function taobaoSearch(value,name){
+    var url = "";
+    if(name=="goods"){
+        if(value!=""){
+            url="https://s.taobao.com/search?q="+value;
+        }else{
+            url="https://www.taobao.com/";
+        }
+    }
+    if(name=="shops"){
+        if(value!=""){
+            url="https://shopsearch.taobao.com/search?app=shopsearch&q="+value;
+        }else{
+            url="https://www.taobao.com/";
+        }
+    }
+    window.open(url,"_blank");
+}
+
+function jdSearch(value,name){
+    var url = "";
+    if(value!=""){
+            url="https://search.jd.com/Search?keyword="+value;
+        }else{
+            url="https://www.jd.com/";
+    }
+
+    window.open(url,"_blank");
+}
+
+function crazySearch(value,name){
+    var url = "";
+    if(value!=""){
+            url="http://ifkdy.com/?q="+value;
+    }else{
+            url="http://ifkdy.com/";
+    }
+    window.open(url,"_blank");
+}
+
+function btSearch(value,name){
+    var url = "";
+    if(name=="digbt"){
+        if(value!=""){
+            url="http://www.digbt.org/search/"+value+"/?u=y";
+        }else{
+            url="http://www.digbt.org/";
+        }
+    }
+    if(name=="btsou"){
+        if(value!=""){
+            url="http://www.btsou.biz/list/"+value+"/1";
+        }else{
+            url="http://www.btsou.biz";
+        }
+    }
+    window.open(url,"_blank");
+}
+
+/*function panSearch(value,name){
+    var url = "";
+    if(name=="goods"){
+        if(value!=null){
+            url="http://www.57fx.com/search/"+value;
+        }else{
+            url="http://www.57fx.com/";
+        }
+    }
+    window.open(url,"_blank");
+}*/
