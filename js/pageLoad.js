97407ffcb6790508919d932fa6a353f868248bad
Ridup <mu_ran@yahoo.com>
2019-07-29 15:22:25 +0800

    添加我的音乐♪

--- js/pageLoad.js
+++ js/pageLoad.js
@@ -7,110 +7,87 @@
 
 'use strict';
 jQuery(document).ready(function ($) {
-    //加载背景图片
-    if (!window.localStorage) {
-        $.messager.show({
-            title: 'Tips',
-            msg: 'Your browser is not support localStorage !',
-            showType: 'show',
-            style: {
-                right: '',
-                top: document.body.scrollTop + document.documentElement.scrollTop,
-                bottom: ''
-            }
-        });
-    } else {
-        var currentBG = window.localStorage.getItem("currentBG");
-        $(".page").attr("style", currentBG);
-    }
-
-    /*alert("beginning...");
-    var strUrl = "";
-    var NewXmlDoc = loadXML("../xml/config.xml");
-    var NewTmpTag = NewXmlDoc.getElementsByTagName("background-images");
-    var strUrl = NewTmpTag[0].getElementsByTagName("now-image")[0].firstChild.nodeValue;
-    alert(strUrl);*/
-    //元素hover()特效
-    $(".changeImg").hover(function () {
-        $(".changeImg span").css("color", "#1ADACF");
-    }, function () {
-        $(".changeImg span").css("color", "white");
+  //加载背景图片
+  if (!window.localStorage) {
+    $.messager.show({
+      title: 'Tips',
+      msg: 'Your browser is not support localStorage !',
+      showType: 'show',
+      style: {
+        right: '',
+        top: document.body.scrollTop + document.documentElement.scrollTop,
+        bottom: ''
+      }
     });
-    $(".helpTip").hover(function () {
-        $(".helpTip span").css("color", "#1ADACF");
-    }, function () {
-        $(".helpTip span").css("color", "white");
-    });
+  } else {
+    var currentBG = window.localStorage.getItem("currentBG");
+    $(".page").attr("style", currentBG);
+  }
 
+  /*alert("beginning...");
+  var strUrl = "";
+  var NewXmlDoc = loadXML("../xml/config.xml");
+  var NewTmpTag = NewXmlDoc.getElementsByTagName("background-images");
+  var strUrl = NewTmpTag[0].getElementsByTagName("now-image")[0].firstChild.nodeValue;
+  alert(strUrl);*/
+  //元素hover()特效
+  $(".changeImg").hover(function () {
+    $(".changeImg span").css("color", "#1ADACF");
+  }, function () {
+    $(".changeImg span").css("color", "white");
+  });
+  $(".helpTip").hover(function () {
+    $(".helpTip span").css("color", "#1ADACF");
+  }, function () {
+    $(".helpTip span").css("color", "white");
+  });
+  $(".cloudMusic").hover(function () {
+    $(".cloudMusic span").css("color", "#C20C0C");
+  }, function () {
+    $(".cloudMusic span").css("color", "white");
+  });
 
 });
 
-//加载xml文件
-/*function loadXML(xmlFile) {
-        alert("beginning...1");
-
-            var xmlDoc, xmlhttp;
-            if (window.XMLHttpRequest) {//兼容 IE7+, Firefox, Chrome, Opera, Safari
-                xmlhttp = new XMLHttpRequest();
-            }
-            else {//兼容 IE5、IE6
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.open("GET", xmlFile, false);  //使用HTTP GET初始化HTTP请求
-            xmlhttp.send(); //发送HTTP请求并获取HTTP响应
-                alert("beginning...2");
-
-            xmlDoc = xmlhttp.responseXML;  //获取XML文档
-            return xmlDoc;
-}*/
-
 //背景图片切换
 function changeBg() {
-    var randomNumber = parseInt(Math.random() * (4050 - 1 + 1) + 1);
-    var imgBgUrl =
-        "background-image: url('https://img.infinitynewtab.com/wallpaper/" + randomNumber + ".jpg');"
-    $(".page").attr("style", imgBgUrl);
+  var randomNumber = parseInt(Math.random() * (4050 - 1 + 1) + 1);
+  var imgBgUrl =
+    "background-image: url('https://img.infinitynewtab.com/wallpaper/" + randomNumber + ".jpg');"
+  $(".page").attr("style", imgBgUrl);
 
-    //保存图片路径
-    if (!window.localStorage) {
-        $.messager.show({
-            title: 'Tips',
-            msg: 'Your browser is not support localStorage !',
-            showType: 'show',
-            style: {
-                right: '',
-                top: document.body.scrollTop + document.documentElement.scrollTop,
-                bottom: ''
-            }
-        });
-    } else {
-        window.localStorage.setItem("currentBG",imgBgUrl);
-    }
-    // XmlDocument xmlDoc=new XmlDocument();
-    // xmlDoc.Load("config.xml");
-    // XmlNode xn=xmlDoc.SelectSingleNode("background-images");
-    // XmlNodeList xnl=xn.ChildNodes;
-    // alert("message");
-    // XmlNode imgUrl=xnl[0];
-    // alert(imgUrl);
-    // XmlElement xe=(XmlElement)imgUrl;
-    // xe.SetAttribute("now-image",imgBgUrl);
-    // xmlDoc.Save("config.xml");//保存。
-    //
+  //保存图片路径
+  if (!window.localStorage) {
+    $.messager.show({
+      title: 'Tips',
+      msg: 'Your browser is not support localStorage !',
+      showType: 'show',
+      style: {
+        right: '',
+        top: document.body.scrollTop + document.documentElement.scrollTop,
+        bottom: ''
+      }
+    });
+  } else {
+    window.localStorage.setItem("currentBG", imgBgUrl);
+  }
+}
 
-
+// 访问音乐
+function toMyCloudMusic(){
+  window.open("http://118.24.103.162/music/","_blank");
 }
 
 
 window.onload = function () {
-    var input = document.getElementById("_easyui_textbox_input1");
-    input.focus();
+  var input = document.getElementById("_easyui_textbox_input1");
+  input.focus();
 };
 
 
 $(document).keydown(function (event) {
 
-    console.log(event.keyCode);
+  console.log(event.keyCode);
 
 });
 
